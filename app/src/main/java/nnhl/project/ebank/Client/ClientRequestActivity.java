531dd339246f69135a86d9198ea5a877c77e3955
concua <gigajet08092000@gmail.com>
2021-05-20 18:51:19 +0700

    counsellor can get client info

--- app/src/main/java/nnhl/project/ebank/Client/ClientRequestActivity.java
+++ app/src/main/java/nnhl/project/ebank/Client/ClientRequestActivity.java
@@ -3,12 +3,39 @@ package nnhl.project.ebank.Client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import nnhl.project.ebank.R;
-public class ClientRequestActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.EditText;
 
+import nnhl.project.ebank.R;
+public class ClientRequestActivity extends AppCompatActivity implements ClientRequestPresenter.View {
+    EditText edClientName,edReqContent;
+    ClientRequestPresenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.clientrequestview);
+        presenter=new ClientRequestPresenter(this);
+        initRequestComponents();
+    }
+
+    private void initRequestComponents() {
+        edClientName=findViewById(R.id.clientNameEdittext);
+        edReqContent=findViewById(R.id.requestContentEdittext);
+    }
+
+    public void onclick_back(View view) {
+        finish();
+    }
+
+    public void onclick_request(View view) {
+        presenter.request(edClientName.getText().toString(), edReqContent.getText().toString());
+        setContentView(R.layout.clientwaitingview);
+    }
+
+    public void onclick_endrequest(View view) {
+        //End request here...
+
+        setContentView(R.layout.clientrequestview);
+        initRequestComponents();
     }
 }
--- app/src/main/java/nnhl/project/ebank/Const.java
+++ app/src/main/java/nnhl/project/ebank/Const.java
@@ -6,6 +6,8 @@ public class Const {
     public static final String DB_URL="https://snhl-bank-default-rtdb.asia-southeast1.firebasedatabase.app";
     public static final String  TBL_counsellors="Counsellors";
     public static final String TBL_SID="SID";
+    public static final String TBL_WAITING_COUNSELLOR="waiting-counsellors";
+    public static final String TBL_WAITING_CLIENTS="waiting-clients";
     /*
     DB Scheme:
      --SID
