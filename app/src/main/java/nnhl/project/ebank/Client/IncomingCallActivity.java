b097f215666b747bf5a5365e8a2baa8e002bcded
concua <gigajet08092000@gmail.com>
2021-05-23 23:26:26 +0700

    video call v3 release

--- app/src/main/java/nnhl/project/ebank/Client/IncomingCallActivity.java
+++ app/src/main/java/nnhl/project/ebank/Client/IncomingCallActivity.java
@@ -108,7 +108,7 @@ public class IncomingCallActivity extends AppCompatActivity implements IncomingC
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 333) {
-            Global.getInstance().getData().put("FLAGF", Const.TAG_YES);
+            Global.getInstance().getData().put(Const.TAG_CALL_COMPLETE, Const.TAG_YES);
             finish();
         }
     }
